70e2672cb30801f73d9fa71db9edd488be198a84
Erwin Luz Leon <eluz@bancoazteca.com>
2021-04-22 16:49:32 -0500

    se consumen los eventos de JS en webview

--- EventosJS/ViewController.swift
+++ EventosJS/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  EventosJS
-//
-//  Created by edeluz on 22/04/21.
-//
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
 
+    private var navegador: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        let contentController = WKUserContentController();
+        contentController.add(
+            self,
+            name: "callbackHandler"
+        )
 
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+
+        navegador = WKWebView(frame: .zero, configuration: config)
+        navegador.load(URLRequest(url: URL(string: "http://10.51.66.215")!))
+        navegador.navigationDelegate = self
+        view = navegador
+
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if(message.name == "callbackHandler") {
+            guard let mensaje =  message.body as? String else {
+                return
+            }
+            print("Se presiono el boton en la Web -> " + mensaje)
+        }
+    }
 
 }
 
